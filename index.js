8d4ad4500ddd524ac20ea40984ed794d5adde807
Pronothurah <onsongojosphat28@gmail.com>
2022-06-22 20:29:19 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+//function my_Details(f_name, height, country){
+   // console.log('My name is:'+ f_name+ ' I am ' + height + ' From ' + country)
+//}
+
+//alert(my_Details('Onsongo Josphat','6ft','Kenya')');
+
+alert('My name is Onsongo Onditi I am 6 foot tall and I am from Kenya')
